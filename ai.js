27bcf2453d650819af47a7d35e48e6e809ecdbcc
akihubcompany <1109hirokun@gmail.com>
2026-03-11 07:00:46 +0900

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,32 +1,34 @@
 let model;
 
-// AIモデル読み込み
 async function loadAI(){
+
 model = await use.load();
+
 console.log("AI model loaded");
+
 }
 
 loadAI();
 
-// コサイン類似度計算
 function cosineSimilarity(a,b){
+
 return tf.losses.cosineDistance(a,b,0).dataSync()[0];
+
 }
 
-// AI診断
 async function aiDiagnosis(input){
 
 let inputEmbedding = await model.embed([input]);
 
-let results = [];
+let results=[];
 
 for(let d of diseases){
 
-let text = d.symptoms.join(" ");
+let text=d.symptoms.join(" ");
 
-let emb = await model.embed([text]);
+let emb=await model.embed([text]);
 
-let similarity = cosineSimilarity(inputEmbedding,emb);
+let similarity=cosineSimilarity(inputEmbedding,emb);
 
 results.push({
 disease:d,
