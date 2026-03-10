98bd4642f3b43edb680db2b7c56a4f7c16cfa106
akihubcompany <1109hirokun@gmail.com>
2026-03-10 22:46:42 +0900

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,27 @@
+{
+name:"胃腸炎",
+symptoms:["腹痛","下痢","吐き気","発熱"],
+danger:"中",
+treatment:"水分補給、必要なら受診"
+},
 
+{
+name:"偏頭痛",
+symptoms:["頭痛","吐き気","光が眩しい"],
+danger:"低",
+treatment:"暗い場所で休む"
+},
+
+{
+name:"熱中症",
+symptoms:["めまい","吐き気","発汗","高体温"],
+danger:"高",
+treatment:"涼しい場所で休む、水分補給"
+},
+
+{
+name:"扁桃炎",
+symptoms:["喉の痛み","発熱","飲み込みにくい"],
+danger:"中",
+treatment:"受診"
+}
