256b2f41fe584c79d2796836ff5db18c64539c9e
akihubcompany <1109hirokun@gmail.com>
2026-03-10 22:47:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+function emergencyCheck(text){
 
+let emergency=["意識がない","呼吸できない","激しい胸痛"]
+
+for(let e of emergency){
+
+if(text.includes(e)){
+
+alert("緊急症状の可能性があります。119に連絡してください。")
+
+}
+
+}
+
+}
