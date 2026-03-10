422a0aebdcfb7555bd5c416ecef265f96a99bb28
akihubcompany <1109hirokun@gmail.com>
2026-03-11 07:31:25 +0900

    Update database.js

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-let diseases=[
+let diseases = [
 
 {
 name:"風邪",
@@ -18,9 +18,9 @@ name:"インフルエンザ",
 symptoms:[
 "高熱",
 "関節痛",
-"倦怠感",
+"寒気",
 "咳",
-"寒気"
+"倦怠感"
 ],
 danger:"中",
 treatment:"医療機関受診"
@@ -37,31 +37,6 @@ symptoms:[
 ],
 danger:"低",
 treatment:"暗い場所で休む"
-},
-
-{
-name:"胃腸炎",
-symptoms:[
-"腹痛",
-"下痢",
-"吐き気",
-"発熱",
-"お腹が痛い"
-],
-danger:"中",
-treatment:"水分補給、症状が続く場合は受診"
-},
-
-{
-name:"肺炎",
-symptoms:[
-"高熱",
-"咳",
-"呼吸が苦しい",
-"胸が痛い"
-],
-danger:"高",
-treatment:"すぐ医療機関受診"
 }
 
-]
+];
